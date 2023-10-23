db45c1abda9ae1ccf6d0ddcd0885a20fab37ba04
Crihojames <crihojames@gmail.com>
2023-10-24 00:11:59 +0100

    add to the js 1.0

---
+++ index.js
@@ -0,0 +1,30 @@
+const apikey= "9380f402246c7b33f50281bfe99eeae5"
+
+const weatherDataEl =document.getElementById("weather-data")
+
+const cityInputEl =document.getElementById("city-input")
+
+const formEl = document.querySelector("form")
+
+formEl.addEventListener("submit",(event) => {
+    event.preventDefault();
+    const cityValue = cityInputEl.value;
+    getWeatherData(cityValue);
+});
+
+function getWeatherData(cityValue) {
+    try{
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?
+        q=${cityValue}&appid=${apikey}&units=metric`);
+
+        if(!response.ok){
+            throw new Error("Network response was not ok");
+        }
+
+        const data = await response.json();
+
+        console.log(data);
+    }   catch(error){
+        
+    }
+}
